f4cbc7040047558afdef6880608766cfef64a540
Justin Frasier <Justin_Frasier@me.com>
2017-03-28 12:53:51 -0500

    add test for the button listener

--- src/main/java/com/justinfrasier/robot/client/ButtonListener.java
+++ src/main/java/com/justinfrasier/robot/client/ButtonListener.java
@@ -14,4 +14,40 @@ public class ButtonListener {
     public static ActionListener test2(){
         return e -> System.out.println("Test2 works");
     }
+
+    public static ActionListener TurnRight(){
+        return e-> System.out.println("Turn Right");
+    }
+
+    public static ActionListener TurnLeft(){
+        return e -> System.out.println("Turn Left");
+    }
+
+    public static ActionListener Forward(){
+        return e -> System.out.println("Forward");
+    }
+
+    public static ActionListener Backward(){
+        return e -> System.out.println("Backward");
+    }
+
+    public static ActionListener CamUp(){
+        return e -> System.out.println("Cam Up");
+    }
+
+    public static ActionListener CamDown(){
+        return e -> System.out.println("Cam Down");
+    }
+
+    public static ActionListener CamLeft(){
+        return e -> System.out.println("Cam Left");
+    }
+
+    public static ActionListener CamRight(){
+        return e -> System.out.println("Cam Right");
+    }
+
+    public static ActionListener Stop(){
+        return e -> System.out.println("Stop");
+    }
 }
--- src/main/java/com/justinfrasier/robot/client/Buttons.java
+++ src/main/java/com/justinfrasier/robot/client/Buttons.java
@@ -2,67 +2,63 @@ package com.justinfrasier.robot.client;
 
 import javax.swing.*;
 
-/**
- * Created by Justin on 03/27/17.
- */
-public class Buttons {
+class Buttons {
 
-    public static JButton ForwardButton(){
+    static JButton ForwardButton(){
         JButton button = new JButton();
         button.setText("Forward");
-        //TODO Add Action listener
-        button.addActionListener(e -> ButtonListener.test());
+        button.addActionListener(ButtonListener.Forward());
         return button;
     }
-    public static JButton RightButton(){
+    static JButton RightButton(){
         JButton button = new JButton();
         button.setText("Right");
-        //TODO Add Action listener
-        button.addActionListener(ButtonListener.test2());
+        button.addActionListener(ButtonListener.TurnRight());
         return button;
     }
-    public static JButton LeftButton(){
+    static JButton LeftButton(){
         JButton button = new JButton();
         button.setText("Left");
-        //TODO Add Action listener
+        button.addActionListener(ButtonListener.TurnLeft());
         return button;
     }
-    public static JButton BackwardsButton(){
+    static JButton BackwardsButton(){
         JButton button = new JButton();
         button.setText("Backwards");
-        //TODO Add Action listener
+        button.addActionListener(ButtonListener.Backward());
         return button;
     }
 
-    public static JButton CamUp(){
+    static JButton CamUp(){
         JButton button = new JButton();
         button.setText("Cam Up");
-        //TODO Add Action listener
-        return button;
-    }
-    public static JButton CamDown(){
-        JButton button = new JButton();
-        button.setText("Cam Down");
-        //TODO Add Action listener
-        return button;
-    }
-    public static JButton CamRight(){
-        JButton button = new JButton();
-        button.setText("Cam Right");
-        //TODO Add Action listener
-        return button;
-    }
-    public static JButton CamLeft(){
-        JButton button = new JButton();
-        button.setText("Cam Left");
-        //TODO Add Action listener
+        button.addActionListener(ButtonListener.CamUp());
         return button;
     }
 
-    public static JButton StopButton(){
+    static JButton CamDown(){
+        JButton button = new JButton();
+        button.setText("Cam Down");
+        button.addActionListener(ButtonListener.CamDown());
+        return button;
+    }
+    static JButton CamRight(){
+        JButton button = new JButton();
+        button.setText("Cam Right");
+        button.addActionListener(ButtonListener.CamRight());
+        return button;
+    }
+    static JButton CamLeft(){
+        JButton button = new JButton();
+        button.setText("Cam Left");
+        button.addActionListener(ButtonListener.CamLeft());
+        return button;
+    }
+
+    static JButton StopButton(){
         JButton button = new JButton();
         button.setText("Stop");
-        //TODO Add Action listener
+        button.addActionListener(ButtonListener.Stop());
         return button;
     }
 
